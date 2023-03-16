12efc9fb0e5509a993b6d78fd0885c53d49b97e8
jasminexu1 <jasminexu0813@gmail.com>
2023-03-16 12:08:06 -0400

    wrap up functions

--- placesApi.js
+++ placesApi.js
@@ -5,6 +5,36 @@ class PlacesAPI {
         this.apiKey = apiKey;
     }
 
+    // extract url to get review dict:
+    /*    text:{author_name,
+                author_url,
+                language,
+                original_language,
+                profile_photo_rul,
+                rating,
+                relative_time_description,
+                text,
+                time,
+                translated}
+    */
+    async getReviewsDict(url) {
+        try {
+            var reviewsDict = {};
+            const name = decodeURIComponent(url.split("/").pop().split("@")[0].replace(/\+/g, " "));
+            const lat = url.split(",")[0].split("@")[1];
+            const lng = url.split(",")[1];
+            const placeId = await placesAPI.getPlaceIdFromLatLng(lat, lng, name);
+            const reviews = await placesAPI.getReviews(placeId);
+            reviews.forEach(review => {
+                reviewsDict[review.text] = review;
+            });
+            return reviewsDict;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
     // GET request to retrieve place_id using lat and lng with correct name
     async getPlaceIdFromLatLng(lat, lng, name) {
         const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=500&type=restaurant&key=${this.apiKey}`;
@@ -19,7 +49,7 @@ class PlacesAPI {
             });
             return res;
         } catch (error) {
-            console.log(error)
+            console.error(error)
         }
         return 0;
     }
@@ -32,28 +62,22 @@ class PlacesAPI {
             const data = await response.json();
             return data.result.reviews;
         } catch (error) {
-            console.log(error)
+            console.error(error)
         }
         return 0;
     }
 }
 
 
-// Sample usage: 
-// apiKey and url might differ
+// apiKey is not expirable, can be used throughout running
 const apiKey = 'AIzaSyA2ft9Ti0ILphcvqQF3qvHvjY35LOhQxdk';
-const url = "https://www.google.com/maps/place/Rose+Bakery+%26+Cafe/@43.4642986,-80.5231153,18z/data=!4m6!3m5!1s0x882b46ae49d69c17:0xd949247f34b9b696!8m2!3d43.55811!4d-79.642758!16s%2Fg%2F1tc_04hc";
 
-// Extract name, lat, lng from url
+// Sample usage: 
 const placesAPI = new PlacesAPI(apiKey);
-const name = decodeURIComponent(url.split("/").pop().split("@")[0].replace(/\+/g, " "));
-const lat = url.split(",")[0].split("@")[1];
-const lng = url.split(",")[1];
-
-// Print review list to console
-placesAPI.getPlaceIdFromLatLng(lat, lng).then(placeId => {
-    placesAPI.getReviews(placeId).then(reviews => {
-        console.log(reviews);
-    });
-});
+const url = "https://www.google.com/maps/place/Rose+Bakery+%26+Cafe/@43.4642986,-80.5231153,18z/data=!4m6!3m5!1s0x882b46ae49d69c17:0xd949247f34b9b696!8m2!3d43.55811!4d-79.642758!16s%2Fg%2F1tc_04hc";
+async function myFunction() {
+    const review = await placesAPI.getReviewsDict(url);
+    console.log(review);
+}
+myFunction();
 
